1205bb345199f2dbc1d1669917708d920837a603
Thigas333 <thiagopereirasousa93@gmail.com>
2026-03-28 22:22:38 -0300

    bug arrumado

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'Dashboard')->name('dashboard');
 });
 
-Route::get('/tela-inicial', [TelaInicialController::class, 'index'])
-    ->name('TelaInicial');
 
 // Route::get('/usuario.store'),
 require __DIR__.'/settings.php';
