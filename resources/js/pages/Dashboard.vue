da3bb6f961b2c8e095d8e9a3a761286c4b03461d
Thigas333 <thiagopereirasousa93@gmail.com>
2026-04-16 17:31:26 -0300

    arrumei uma route

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -15,7 +15,7 @@ const breadcrumbs: BreadcrumbItem[] = [
 
 <template>
     <Head title="Dashboard" />
-
+    <p> grfrf</p>
     <AppLayout :breadcrumbs="breadcrumbs">
         <div
             class="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4"
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
 // Route::get('/Welcome', [TaskViewController::class, 'index'])->name('welcome');
 
 Route::get('/home-tasks', [TaskViewController::class, 'index']);
-Route::get('/login', [TelaInicialController::class, 'index'])->name('home');
+Route::get('/login', [TelaInicialController::class, 'index']);
 // Route::get('usuario.store', [UsuarioController::class, 'store'])->name('usuario.store');
 
 // Route::get('/usuario.store'),
